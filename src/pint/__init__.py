bc88121f4adcb4ed6e1ae0324e2fc534c0560a5d
AbooMinister25 <aboominister@gmail.com>
2024-07-29 16:56:15 -0400

    Started rewrite, create basic Result and Error types

--- src/pint/__init__.py
+++ src/pint/__init__.py
@@ -1,24 +1,37 @@
 """A user friendly parser combinator library for python."""
 
+from __future__ import annotations
+
 __version__ = "0.1.0"
 
+from typing import Generic, NamedTuple, TypeVar
 
-__version__ = "0.1.0"
+Input = TypeVar("Input")
+Output = TypeVar("Output")
 
-from pint.core import ParseFunction, ParseResult
-from pint.errors import CustomError, ParseError, UnclosedError, UnexpectedError
-from pint.parsers import Parser, ident, symbol, take_or_not, take_while
 
-__all__ = [
-    "ParseFunction",
-    "ParseResult",
-    "CustomError",
-    "ParseError",
-    "UnclosedError",
-    "UnexpectedError",
-    "Parser",
-    "ident",
-    "symbol",
-    "take_or_not",
-    "take_while",
-]
+class Error:
+    """The default error type. It only contains an error message."""
+
+    def __init__(self, message: str) -> None:
+        """Creates a new Error.
+
+        Args:
+            message (str): The error message.
+        """
+        self.message = message
+
+
+class Result(NamedTuple, Generic[Input, Output]):
+    """Holds the result of a parsing function.
+
+    A Result is a tuple consisting of an input of type Input and an output of type
+    Output.
+
+    Attributes:
+        input (Input): The remainder of the input after parsing.
+        output (Output): The parsed output.
+    """
+
+    input: Input
+    output: Output
